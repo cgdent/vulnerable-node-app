0262c63cedda7aea5405e5c8fa821e9a2d6af6f3
mike <michaelbraunbass@gmail.com>
2023-05-05 17:03:19 -0600

    Added K8s

--- app/db.js
+++ app/db.js
@@ -1,5 +1,5 @@
 let mongo_port = process.env.MONGO_PORT || 27017;
-let mongo_host = process.env.MONGO_HOST || "localhost";
+let mongo_host = process.env.MONGO_HOST || "mongo";
 
 module.exports = {
 	DB: 'mongodb://' + mongo_host + ':' + mongo_port + '/vulnerablemean'
